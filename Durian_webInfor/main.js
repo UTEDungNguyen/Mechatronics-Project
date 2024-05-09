10d2d0def3c3e886d2a55bd8c1cfdea7bd5aa9ad
VietHa <havietnguyen2002@gmail.com>
2024-05-09 18:00:40 +0700

    update main.js

--- Durian_webInfor/main.js
+++ Durian_webInfor/main.js
@@ -1,52 +1,31 @@
-var valueTemp = firebase.database().ref("Day");
-valueTemp.on("value", (snap) => {
+var valueDayExport = firebase.database().ref("sample_1").child("date of exportation");
+// valueTemp.on("value", (snap) => {
   console.log("Day: " + snap.val());
-  document.getElementById("day").innerHTML = snap.val() + " days";
-});
+  document.getElementById("day").innerHTML = snap.val();
+// });
 
-var valueStage = firebase.database().ref("grown_up");
-valueStage.on("value", (snap) => {
-  if (snap.val() == 0) {
-    console.log("Stage: cây non  ");
-    document.getElementById("plant_stage").innerHTML = "Immature";
-  } else {
-    console.log("Stage: cây non  ");
-    document.getElementById("plant_stage").innerHTML = " Mature";
-  }
-});
 
-var valueTemp = firebase.database().ref("Value").child("temp_air");
-valueTemp.on("value", (snap) => {
-  console.log("nhiet do: " + snap.val());
-  document.getElementById("temperature").innerHTML = snap.val() + " °C";
-});
 
-var valueHum = firebase.database().ref("Value").child("hum");
-valueHum.on("value", (snap) => {
-  console.log("do am: " + snap.val());
-  document.getElementById("humidity").innerHTML = snap.val() + " %";
-});
+var valueOrgin = firebase.database().ref("sample_1").child("orgin");
+// valueTemp.on("value", (snap) => {
+  console.log("the orgin: " + snap.val());
+  document.getElementById("orgin").innerHTML = snap.val() ;
+// });
 
-var valueHum = firebase.database().ref("Value").child("temp_water");
-valueHum.on("value", (snap) => {
-  console.log("nhiet do nuoc: " + snap.val());
-  document.getElementById("temperature_water").innerHTML = snap.val() + " °C";
-});
+var valueHum = firebase.database().ref("sample_1").child("durian's name");
+// valueHum.on("value", (snap) => {
+  console.log("durian's name: " + snap.val());
+  document.getElementById("name").innerHTML = snap.val() ;
+// });
 
-var valuepH = firebase.database().ref("Value").child("pH");
-valuepH.on("value", (snap) => {
-  console.log("pH: " + snap.val());
-  document.getElementById("pH").innerHTML = snap.val() + " pH";
-});
+var valueHum = firebase.database().ref("sample_1").child("weight");
+// valueHum.on("value", (snap) => {
+  console.log("trọng lượng: " + snap.val());
+  document.getElementById("weight").innerHTML = snap.val() ;
+// });
 
-var valueppm = firebase.database().ref("Value").child("ppm");
-valueppm.on("value", (snap) => {
-  console.log("ppm: " + snap.val());
-  document.getElementById("ppm").innerHTML = snap.val() + " ppm";
-});
-
-var valueppm = firebase.database().ref("Value").child("lux");
-valueppm.on("value", (snap) => {
-  console.log("lux: " + snap.val());
-  document.getElementById("light_int").innerHTML = snap.val() + " lux";
-});
+var valuepH = firebase.database().ref("sample_1").child("type");
+// valuepH.on("value", (snap) => {
+  console.log("Type: " + snap.val());
+  document.getElementById("type").innerHTML = snap.val() ;
+// });
