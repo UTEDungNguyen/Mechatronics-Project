ae85c07cd07393470189a914e6c4bd9f5904d220
Binh Nguyen <anhbinhnguyen27042002@gmail.com>
2024-05-18 18:09:32 +0700

    update show image

--- GetResultSample.py
+++ GetResultSample.py
@@ -354,9 +354,6 @@ while True:
                 # SHOW THE IMAGE IN TERMINAL
                 imgstack = stackImages(0.8,([image_original,image_original],[object.getResultObject(image_original),defect.getResultDefect(image_original)]))
                 cv2.imshow("The Image of the Project",imgstack)
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    cv2.destroyAllWindows()
-                    
                 if resultObject == True and resultDefect == False:
                     print("########################### Meet Standard IMG Processing ##############")
                     MeetStandardIMGProcessing = True
