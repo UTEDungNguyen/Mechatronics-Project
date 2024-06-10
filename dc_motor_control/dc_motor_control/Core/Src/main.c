ff7c601cd387047e72a86fa30abb33c2fb644f22
Dung Nguyen <dungduide2002@gmail.com>
2024-06-11 04:48:03 +0700

    update: implement classify dc motor control

--- dc_motor_control/dc_motor_control/Core/Src/main.c
+++ dc_motor_control/dc_motor_control/Core/Src/main.c
@@ -36,8 +36,9 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 #define RXBUF_SIZE 8
-#define DATA_PWM_SIZE 3
+#define DATA_PWM_SIZE 6
 #define DUTY_CYCLE_SIZE 2
+#define CLASSIFY_PWM_T 50
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -59,17 +60,28 @@ DMA_HandleTypeDef hdma_usart2_rx;
 DMA_HandleTypeDef hdma_usart2_tx;
 
 /* USER CODE BEGIN PV */
-uint8_t rxbuf[RXBUF_SIZE];
-uint8_t rxbuf_2[RXBUF_SIZE];
-uint16_t pwm_t = 0;
-uint16_t state = 0;
-uint16_t state_change = 0;
-uint8_t pwm_test_1 = 0;
-uint8_t pwm_test_2 = 0;
-uint8_t test_state = 0;
-char data_pwm[DATA_PWM_SIZE];
-char duty_cycle_send[DUTY_CYCLE_SIZE];
+// Define Buffer receive UART
+static uint8_t rxbuf[RXBUF_SIZE];
+static uint8_t rxbuf_2[RXBUF_SIZE];
 
+// Variable
+static uint16_t pwm_t = 0;
+static uint8_t state = 0;
+static uint8_t classify_state = 0;
+static uint8_t interrupt_timer_state = 0;
+static uint8_t otherwise_state = 0;
+static uint8_t run_classify = 0;
+static uint16_t state_change = 0;
+
+static uint8_t pwm_test_1 = 0;
+static uint8_t pwm_test_2 = 0;
+static uint8_t test_state = 0;
+
+// Define Buffer analyze PWM
+static char data_pwm[DATA_PWM_SIZE];
+static char duty_cycle_send[DUTY_CYCLE_SIZE];
+
+// Define Enum of Conveyor DC Motor State
 typedef enum
 {
   STOP_MOTOR_STATE,
@@ -77,6 +89,64 @@ typedef enum
   MOTOR_LEFT_STATE,
   MOTOR_RIGHT_STATE
 } dc_motor_state_t;
+
+// Define Enum of Conveyor DC Motor Status
+typedef enum
+{
+  CONVEYOR_STATUS_OK = 0x00, /*!< Everything OK */
+  CONVEYOR_STATUS_ERROR
+} conveyor_status_t;
+
+// Define Enum of Classify DC Motor State
+typedef enum
+{
+  ORIGINAL_CLASSIFY_STATE,
+  RIGHT_CLASSIFY_STATE,
+  LEFT_CLASSIFY_STATE
+} classify_state_t;
+
+// Define Enum of Classify Status Running or Stop
+typedef enum
+{
+  START_CLASSIFY_STATE,
+  STOP_CLASSIFY_STATE
+} running_classify_state_t;
+
+// Define Enum of Conveyor DC Motor Status
+typedef enum
+{
+  CLASSIFY_STATUS_OK = 0x00, /*!< Everything OK */
+  CLASSIFY_STATUS_ERROR
+} classify_status_t;
+
+// Define Enum UART Transmit Status
+typedef enum
+{
+  UART_TRANSMIT_STATUS_OK = 0x00, /*!< Everything OK */
+  UART_TRANSMIT_STATUS_ERROR
+} uart_transmit_status_t;
+
+// Define Enum UART Variable Receive Status
+typedef enum
+{
+  UART_VARIABLE_RX_STATUS_OK = 0x00, /*!< Everything OK */
+  UART_VARIABLE_RX_STATUS_DONE
+} uart_variable_rx_status_t;
+
+// Define Enum Interrupt Timer Status
+typedef enum
+{
+  START_INTERRUPT_TIMER_STATUS = 0x00, /*!< Everything OK */
+  RUNNING_INTERRUPT_TIMER_STATUS,
+  STOP_INTERRUPT_TIMER_STATUS
+} interrupt_timer_status_t;
+
+// Define Enum Check Interrupt Timer Status
+typedef enum
+{
+  START_OTHERWISE_STATUS = 0x00, /*!< Everything OK */
+  STOP_OTHERWISE_STATUS
+} interrupt_otherwise_status_t;
 
 /* USER CODE END PV */
 
@@ -91,16 +161,47 @@ static void MX_USART2_UART_Init(void);
 static void MX_TIM3_Init(void);
 static void MX_TIM4_Init(void);
 /* USER CODE BEGIN PFP */
-
+static uart_transmit_status_t uart_transmit();
+static conveyor_status_t conveyor_control(uint8_t state, uint8_t pwm_t);
+static classify_status_t classify_control(uint8_t state, uint8_t pwm_classify);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* Prevent unused argument(s) compilation warning */
+  UNUSED(htim);
+
+  if (interrupt_timer_state == RUNNING_INTERRUPT_TIMER_STATUS)
+  {
+    if (otherwise_state == START_OTHERWISE_STATUS)
+    {
+      classify_state = classify_state == LEFT_CLASSIFY_STATE ? RIGHT_CLASSIFY_STATE : LEFT_CLASSIFY_STATE;
+      interrupt_timer_state = START_INTERRUPT_TIMER_STATUS;
+      otherwise_state = STOP_OTHERWISE_STATUS;
+      HAL_TIM_Base_Stop_IT(&htim3);
+    }
+    else
+    {
+      run_classify = STOP_CLASSIFY_STATE;
+      classify_state = ORIGINAL_CLASSIFY_STATE;
+      interrupt_timer_state = STOP_INTERRUPT_TIMER_STATUS;
+      otherwise_state = START_OTHERWISE_STATUS;
+      HAL_TIM_Base_Stop_IT(&htim3);
+    }
+  }
+
+  /* NOTE : This function should not be modified, when the callback is needed,
+            the HAL_TIM_PeriodElapsedCallback could be implemented in the user file
+   */
+}
+
 void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
 {
   if (huart->Instance == USART1)
   {
-	state_change = 0;
+    state_change = UART_VARIABLE_RX_STATUS_OK;
     switch (rxbuf[0])
     {
     case 'S':
@@ -146,17 +247,30 @@ void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
 
   if (huart->Instance == USART2)
   {
-	  state = MOTOR_LEFT_STATE;
-	  state_change = 0;
-      for (int b = 0; b <= 1; b++)
-      {
-        data_pwm[b] = rxbuf_2[b];
-      }
+    for (int b = 0; b < Size; b++)
+    {
+      data_pwm[b] = rxbuf_2[b];
+    }
+
+    if (data_pwm[0] != 'L' || data_pwm[0] != 'R')
+    {
+      state = MOTOR_LEFT_STATE;
+      state_change = UART_VARIABLE_RX_STATUS_OK;
       pwm_t = atoi(data_pwm);
-      for (uint8_t i = 0U; i < Size; i++)
-      {
-        rxbuf_2[i] = '\0';
-      }
+    }
+
+    if (data_pwm[0] == 'L' || data_pwm[0] == 'R')
+    {
+      run_classify = START_CLASSIFY_STATE;
+      interrupt_timer_state = START_INTERRUPT_TIMER_STATUS;
+      classify_state = data_pwm[0] == 'L' ? LEFT_CLASSIFY_STATE : RIGHT_CLASSIFY_STATE;
+    }
+
+    for (uint8_t i = 0U; i < Size; i++)
+    {
+      data_pwm[i] = '\0';
+      rxbuf_2[i] = '\0';
+    }
     /* start the DMA again */
     HAL_UARTEx_ReceiveToIdle_DMA(&huart2, (uint8_t *) rxbuf_2, RXBUF_SIZE);
     __HAL_DMA_DISABLE_IT(&hdma_usart2_rx, DMA_IT_HT);
@@ -165,9 +279,9 @@ void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
   /* USER CODE BEGIN 1 */
@@ -213,8 +327,6 @@ int main(void)
   HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_2);
   HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_3);
   HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_4);
-//  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
-//  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -224,44 +336,32 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-//  HAL_UART_Transmit(&huart2, (uint8_t *) "dungdeptrai", sizeof("hello dungdeptrai"), 100);
-
-    switch (state)
+    if (conveyor_control(state, pwm_t) != CONVEYOR_STATUS_OK)
     {
-    case STOP_MOTOR_STATE:
-    {
-      pwm_t = 0;
-      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, pwm_t);
-      __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, pwm_t);
-      break;
-    }
-    case START_MOTOR_STATE:
-    {
-      pwm_t = 49;
-      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, 0);
-      __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, pwm_t);
-      break;
-    }
-    case MOTOR_LEFT_STATE:
-    {
-      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, 0);
-      __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, pwm_t);
-      break;
-    }
-    case MOTOR_RIGHT_STATE:
-    {
-      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, pwm_t);
-      __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, 0);
-      break;
-    }
+      Error_Handler();
     }
 
-    if (state_change == 0)
+    if (state_change == UART_TRANSMIT_STATUS_OK)
     {
-    	sprintf(duty_cycle_send, "%2d", pwm_t);
-		/* Transmit UART of Duty Cycle Polling */
-      HAL_UART_Transmit(&huart2, (uint8_t *) duty_cycle_send, sizeof(duty_cycle_send), 100);
-      state_change = 1;
+      if (uart_transmit() != UART_TRANSMIT_STATUS_OK)
+      {
+        Error_Handler();
+      }
+      state_change = UART_VARIABLE_RX_STATUS_DONE;
+    }
+
+    if (run_classify == START_CLASSIFY_STATE)
+    {
+      if (interrupt_timer_state == START_INTERRUPT_TIMER_STATUS)
+      {
+        HAL_TIM_Base_Start_IT(&htim3);
+        interrupt_timer_state = RUNNING_INTERRUPT_TIMER_STATUS;
+      }
+      if (classify_control(classify_state, CLASSIFY_PWM_T) != CLASSIFY_STATUS_OK)
+      {
+        Error_Handler();
+      }
+      // run_classify = STOP_CLASSIFY_STATE;
     }
 
     if (test_state)
@@ -275,31 +375,26 @@ int main(void)
       __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, pwm_test_1);
       __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, pwm_test_1);
     }
-//    sprintf(duty_cycle_send, "Duty Cycle Of DC Motor = %d \n", pwm_t);
-    /* Transmit UART of Duty Cycle Polling */
-//    HAL_UART_Transmit(&huart1, (uint8_t *) duty_cycle_send, sizeof(duty_cycle_send), 100);
-    /* Transmit UART of Duty Cycle DMA */
-    // HAL_UART_Transmit_DMA(&huart1, (uint8_t *) duty_cycle_send, sizeof(duty_cycle_send));
   }
   /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -309,9 +404,9 @@ void SystemClock_Config(void)
     Error_Handler();
   }
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType =
+    RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
@@ -324,21 +419,20 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief TIM1 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief TIM1 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_TIM1_Init(void)
 {
-
   /* USER CODE BEGIN TIM1_Init 0 */
 
   /* USER CODE END TIM1_Init 0 */
 
-  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-  TIM_OC_InitTypeDef sConfigOC = {0};
-  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
+  TIM_ClockConfigTypeDef sClockSourceConfig = { 0 };
+  TIM_MasterConfigTypeDef sMasterConfig = { 0 };
+  TIM_OC_InitTypeDef sConfigOC = { 0 };
+  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = { 0 };
 
   /* USER CODE BEGIN TIM1_Init 1 */
 
@@ -399,24 +493,22 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 2 */
   HAL_TIM_MspPostInit(&htim1);
-
 }
 
 /**
-  * @brief TIM2 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief TIM2 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_TIM2_Init(void)
 {
-
   /* USER CODE BEGIN TIM2_Init 0 */
 
   /* USER CODE END TIM2_Init 0 */
 
-  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-  TIM_OC_InitTypeDef sConfigOC = {0};
+  TIM_ClockConfigTypeDef sClockSourceConfig = { 0 };
+  TIM_MasterConfigTypeDef sMasterConfig = { 0 };
+  TIM_OC_InitTypeDef sConfigOC = { 0 };
 
   /* USER CODE BEGIN TIM2_Init 1 */
 
@@ -466,23 +558,21 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 2 */
   HAL_TIM_MspPostInit(&htim2);
-
 }
 
 /**
-  * @brief TIM3 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief TIM3 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_TIM3_Init(void)
 {
-
   /* USER CODE BEGIN TIM3_Init 0 */
 
   /* USER CODE END TIM3_Init 0 */
 
-  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_ClockConfigTypeDef sClockSourceConfig = { 0 };
+  TIM_MasterConfigTypeDef sMasterConfig = { 0 };
 
   /* USER CODE BEGIN TIM3_Init 1 */
 
@@ -511,24 +601,22 @@ static void MX_TIM3_Init(void)
   /* USER CODE BEGIN TIM3_Init 2 */
 
   /* USER CODE END TIM3_Init 2 */
-
 }
 
 /**
-  * @brief TIM4 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief TIM4 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_TIM4_Init(void)
 {
-
   /* USER CODE BEGIN TIM4_Init 0 */
 
   /* USER CODE END TIM4_Init 0 */
 
-  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-  TIM_OC_InitTypeDef sConfigOC = {0};
+  TIM_ClockConfigTypeDef sClockSourceConfig = { 0 };
+  TIM_MasterConfigTypeDef sMasterConfig = { 0 };
+  TIM_OC_InitTypeDef sConfigOC = { 0 };
 
   /* USER CODE BEGIN TIM4_Init 1 */
 
@@ -582,17 +670,15 @@ static void MX_TIM4_Init(void)
 
   /* USER CODE END TIM4_Init 2 */
   HAL_TIM_MspPostInit(&htim4);
-
 }
 
 /**
-  * @brief USART1 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief USART1 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_USART1_UART_Init(void)
 {
-
   /* USER CODE BEGIN USART1_Init 0 */
 
   /* USER CODE END USART1_Init 0 */
@@ -615,17 +701,15 @@ static void MX_USART1_UART_Init(void)
   /* USER CODE BEGIN USART1_Init 2 */
 
   /* USER CODE END USART1_Init 2 */
-
 }
 
 /**
-  * @brief USART2 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief USART2 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_USART2_UART_Init(void)
 {
-
   /* USER CODE BEGIN USART2_Init 0 */
 
   /* USER CODE END USART2_Init 0 */
@@ -648,15 +732,13 @@ static void MX_USART2_UART_Init(void)
   /* USER CODE BEGIN USART2_Init 2 */
 
   /* USER CODE END USART2_Init 2 */
-
 }
 
 /**
-  * Enable DMA controller clock
-  */
+ * Enable DMA controller clock
+ */
 static void MX_DMA_Init(void)
 {
-
   /* DMA controller clock enable */
   __HAL_RCC_DMA2_CLK_ENABLE();
   __HAL_RCC_DMA1_CLK_ENABLE();
@@ -674,42 +756,105 @@ static void MX_DMA_Init(void)
   /* DMA2_Stream7_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA2_Stream7_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(DMA2_Stream7_IRQn);
-
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_GPIO_Init(void)
 {
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  GPIO_InitTypeDef GPIO_InitStruct = { 0 };
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5|GPIO_PIN_6, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5 | GPIO_PIN_6, GPIO_PIN_RESET);
 
   /*Configure GPIO pins : PA5 PA6 */
-  GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_6;
+  GPIO_InitStruct.Pin = GPIO_PIN_5 | GPIO_PIN_6;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
 }
 
 /* USER CODE BEGIN 4 */
+static uart_transmit_status_t uart_transmit()
+{
+  sprintf(duty_cycle_send, "%2d", pwm_t);
+  /* Transmit UART of Duty Cycle Polling */
+  HAL_UART_Transmit(&huart2, (uint8_t *) duty_cycle_send, sizeof(duty_cycle_send), 100);
+  return UART_TRANSMIT_STATUS_OK;
+}
+static conveyor_status_t conveyor_control(uint8_t state, uint8_t pwm_t)
+{
+  switch (state)
+  {
+  case STOP_MOTOR_STATE:
+  {
+    pwm_t = 0;
+    __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, pwm_t);
+    __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_1, pwm_t);
+    break;
+  }
+  case START_MOTOR_STATE:
+  {
+    pwm_t = 49;
+    __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, 0);
+    __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_1, pwm_t);
+    break;
+  }
+  case MOTOR_LEFT_STATE:
+  {
+    __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, 0);
+    __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_1, pwm_t);
+    break;
+  }
+  case MOTOR_RIGHT_STATE:
+  {
+    __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, pwm_t);
+    __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_1, 0);
+    break;
+  }
+    return CONVEYOR_STATUS_OK;
+  }
+}
 
+static classify_status_t classify_control(uint8_t classify_state, uint8_t pwm_classify)
+{
+  switch (classify_state)
+  {
+  case ORIGINAL_CLASSIFY_STATE:
+  {
+    __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_3, 0);
+    __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_4, 0);
+    break;
+  }
+  case LEFT_CLASSIFY_STATE:
+  {
+    __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_3, pwm_classify);
+    __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_4, 0);
+    break;
+  }
+  case RIGHT_CLASSIFY_STATE:
+  {
+    __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_3, 0);
+    __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_4, pwm_classify);
+    break;
+  }
+    return CLASSIFY_STATUS_OK;
+  }
+}
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -721,14 +866,14 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
