cf4e6eee22b5d931725fbd11dd71ac4d7e8637d5
Dung Nguyen <dungduide2002@gmail.com>
2024-06-27 20:15:49 +0700

    update: fix bug when adding two sensor

--- dc_motor_control/dc_motor_control/Core/Src/main.c
+++ dc_motor_control/dc_motor_control/Core/Src/main.c
@@ -597,7 +597,7 @@ static void MX_TIM3_Init(void)
   htim3.Instance = TIM3;
   htim3.Init.Prescaler = 1599;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 3799;
+  htim3.Init.Period = 4499;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
